be7c3d11e9ba45576b9901de3aa152339dc78c6a
yassermembreno <yass.membreo.gudiel@gmail.com>
2022-03-20 09:23:07 -0600

    Added insert command to create two roles in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into ROLE(role_name, description, created_on, modified_on) 
+          values('ADMIN', 'User with administration profile', current_timestamp, current_timestamp),
+                ('EXTERNAL', 'User with common access profile' , current_timestamp, current_timestamp) 
+
